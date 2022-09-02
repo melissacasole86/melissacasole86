d722ac2369c0208a7c3e877bbe746c0905019ce8
Melissa Casole <melissacasole@Wavicles-MBP.lan>
2022-09-02 17:55:54 -0400

    first add

---
+++ main.js
@@ -0,0 +1,14 @@
+import "./src/components/card/card.css";
+import "./tokens.css";
+import "./main.css";
+
+// keeping this for future reference
+
+// import javascriptLogo from "./javascript.svg";
+// import { setupCounter } from "./counter.js";
+
+// document.querySelector("#app").innerHTML = `
+//     <h1>Hello Vite!</h1>
+// `;
+
+// setupCounter(document.querySelector("#counter"));
